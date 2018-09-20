964bb116a81bb84faf45f39cf0dd43dd43984e48
Carol Greene <carolgreene3@gmail.com>
2018-09-20 00:51:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 function getFirstSelector (selector) {
-  return document.querySelector(`${selector}`)
+  return document.querySelector(selector)
 }
 
+//can also do
+//function getFirstSelector (selector) {
+  //return document.querySelector(`${selector}`)
+//}
+
 function nestedTarget () {
-  return document.querySelector(`#nested .target`)
+  return document.querySelector('#nested .target')  //# for id & . for class
 }
 
 function increaseRankBy (n) {
   const lis = document.querySelectorAll('ul.ranked-list li')
 
   for (let i = 0; i < lis.length; i++) {
-   lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
-  }
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+    }
 }
 
-function deepestChild() {
-  const node = document.querySelector("div#grand-node")
+function deepestChild () {
+  const node = document.querySelector('#grand-node')
   const div = node.querySelectorAll('div')[3]
   return div
 }
+
+//can also do
+//function deepestChild () {
+//let node = document.getElementById('grand-node')
+//let nextNode = node.children[0]
+
+//while (nextNode) {
+//node = nextNode
+//nextNode = node.children[0]
+//}
+//return node
+//}
