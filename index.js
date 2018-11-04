591e2ef6dd4bdb2978869449d920b7586c80ab7b
Carol Greene <carolgreene3@gmail.com>
2018-11-04 23:53:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 
 function getFirstSelector(selector) {
-  return document.querySelector(selector)
+  return document.querySelector(selector)  //querySelector will give us the first match
 }
 
 function nestedTarget() {
-  return document.querySelector('#nested .target')
+  return document.querySelector('#nested .target')   //# stands for div,   . stands for className
 }
 
 function increaseRankBy(n) {
-  const lis = document.querySelectorAll('.ranked-list li')
+  const lis = document.querySelectorAll('.ranked-list li')  //set variable for all ranked-list li's
 
-  for(let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = parseInt(lis[i].innerHTML)+ n
-  }
-}
+  for(let i = 0; i < lis.length; i++) {                   //iterate thru the variable
+    lis[i].innerHTML = parseInt(lis[i].innerHTML)+ n     //set new li innerHTML value to the old value + n
+  }                                                     //use parseInt bc value in li is string & you Have
+}                                                       //to add n to it
 
+//function deepestChild() {
+  //const node = document.getElementById('grand-node')    //set variable for grand node element
+  //const div = node.querySelectorAll('div')[3]           //set variable for 4th div in grand node
+  //return div                                            //return the div
+//}
+
+//or can do
 function deepestChild() {
-  const node = document.getElementById('grand-node')
-  const div = node.querySelectorAll('div')[3]
-  return div
+  let node = document.getElementsById('grand-node') 
+  let nextNode = node.children[0] 
+
+  while (nextNode) {
+    node = nextNode 
+    nextNode = node.children[0]
+  }
+  return node
 }
