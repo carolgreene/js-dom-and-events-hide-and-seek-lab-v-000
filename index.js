95e88921fad388950c0b1d60ae4cebe736f91061
Carol Greene <carolgreene3@gmail.com>
2018-11-04 23:11:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,38 +1,22 @@
-function getFirstSelector (selector) {
+
+function getFirstSelector(selector) {
   return document.querySelector(selector)
 }
 
-//can also do
-//function getFirstSelector (selector) {
-  //return document.querySelector(`${selector}`)
-//}
-
-function nestedTarget () {
-  return document.querySelector('#nested .target')  //# for id & . for class
+function nestedTarget() {
+  return document.querySelector('#nested .target')
 }
 
-function increaseRankBy (n) {
-  const lis = document.querySelectorAll('ul.ranked-list li')
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('.ranked-list li')
 
-  for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
-    }
+  for(let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML)+ n
+  }
 }
 
-function deepestChild () {
-  const node = document.querySelector('#grand-node')
+function deepestChild() {
+  const node = document.getElementById('grand-node')
   const div = node.querySelectorAll('div')[3]
   return div
 }
-
-//can also do
-//function deepestChild () {
-//let node = document.getElementById('grand-node')
-//let nextNode = node.children[0]
-
-//while (nextNode) {
-//node = nextNode
-//nextNode = node.children[0]
-//}
-//return node
-//}
