ecef846c00f7642c77abc35a4e005740776b1920
Carol Greene <carolgreene3@gmail.com>
2018-11-04 23:54:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
-function getFirstSelector (selector) {
-  return document.querySelector(selector)
+
+function getFirstSelector(selector) {
+  return document.querySelector(selector)  //querySelector will give us the first match
 }
 
-//can also do
-//function getFirstSelector (selector) {
-  //return document.querySelector(`${selector}`)
-//}
-
-function nestedTarget () {
-  return document.querySelector('#nested .target')  //# for id & . for class
+function nestedTarget() {
+  return document.querySelector('#nested .target')   //# stands for div,   . stands for className
 }
 
-function increaseRankBy (n) {
-  const lis = document.querySelectorAll('ul.ranked-list li')
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('.ranked-list li')  //set variable for all ranked-list li's
 
-  for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
-    }
+  for(let i = 0; i < lis.length; i++) {                   //iterate thru the variable
+    lis[i].innerHTML = parseInt(lis[i].innerHTML)+ n     //set new li innerHTML value to the old value + n
+  }                                                     //use parseInt bc value in li is string & you Have
+}                                                       //to add n to it
+
+function deepestChild() {
+  const node = document.getElementById('grand-node')    //set variable for grand node element
+  const div = node.querySelectorAll('div')[3]           //set variable for 4th div in grand node
+  return div                                            //return the div
 }
 
-function deepestChild () {
-  const node = document.querySelector('#grand-node')
-  const div = node.querySelectorAll('div')[3]
-  return div
-}
+//or can do
+//function deepestChild() {
+  //let node = document.getElementById('grand-node')
+  //let nextNode = node.children[0]
 
-//can also do
-//function deepestChild () {
-//let node = document.getElementById('grand-node')
-//let nextNode = node.children[0]
-
-//while (nextNode) {
-//node = nextNode
-//nextNode = node.children[0]
-//}
-//return node
+  //while (nextNode) {
+  //  node = nextNode
+  //  nextNode = node.children[0]
+  //}
+  //return node
 //}
